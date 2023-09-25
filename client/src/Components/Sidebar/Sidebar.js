689d74c21a1d200fa474b69c24ad833d1c41087b
Pradyumna Singh <pradyumnasingh@Pradyumnas-MacBook-Air.local>
2023-09-26 05:01:44 +0530

    sidebar

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Sidebar.css";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import { FaHome, FaPlus, FaListUl, FaSignInAlt, FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      name: "Home",
+      to: "/home",
+      icon: <FaHome />,
+    },
+    {
+      name: "Create",
+      to: "/create",
+      icon: <FaPlus />,
+    },
+    {
+      name: "My List",
+      to: "/list",
+      icon: <FaListUl />,
+    },
+    {
+      name: "Auth",
+      to: "/",
+      icon: <FaSignInAlt />,
+    },
+  ];
+  return (
+    <div className="container">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h3 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            flow
+          </h3>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.to}
+            key={index}
+            className="link"
+            activeclassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default Sidebar;
