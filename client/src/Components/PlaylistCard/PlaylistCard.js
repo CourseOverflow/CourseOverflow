70dbd18de68ece6cc74cc9f1ea7a87df5623272e
Pradyumna Singh <pradyumnasingh@Pradyumnas-MacBook-Air.local>
2023-10-23 18:35:01 +0530

    stage correction2

--- client/src/Components/PlaylistCard/PlaylistCard.js
+++ client/src/Components/PlaylistCard/PlaylistCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./PlayListCard.module.css";
+import styles from "./PlaylistCard.module.css";
 const PlayListCard = (props) => {
   return (
     <div className={styles.container}>
