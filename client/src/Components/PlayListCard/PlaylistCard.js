a16025ef3333abae750c971847db780c5640ff42
AnshUjlayan <anshujlayan3@gmail.com>
2023-10-23 18:44:51 +0530

    trash 2

---
+++ client/src/Components/PlayListCard/PlaylistCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./PlaylistCard.module.css";
+import { FaPlay } from "react-icons/fa";
+import Checkbox from "./Checkbox";
+
+const PlaylistCard = (props) => {
+  return (
+    <div className={`${styles.container} ${props.isWatched && styles.played}`}>
+      <div
+        onClick={props.clickHandler}
+        className={`${styles.playlistCard} ${props.isWatched && styles.played}`}
+      >
+        <span className={`${styles.videoIdx}`}>
+          {props.currVideo ? <FaPlay /> : props.rank}
+        </span>
+        <div className={`${styles.imageContainer}`}>
+          <img
+            className={`${styles.thumbnail}`}
+            src={props.thumbnail}
+            alt={props.topic}
+          />
+          <p className={`${styles.duration}`}>{props.duration}</p>{" "}
+        </div>
+        <div className={`${styles.details}`}>
+          <h5 className={`${styles.topic}`}>{props.topic}</h5>
+          <p className={`${styles.author}`}>{props.author}</p>
+        </div>
+      </div>
+      {props.playlistItem && (
+        <Checkbox
+          checkboxHandler={props.checkboxHandler}
+          watched={props.isWatched}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PlaylistCard;
