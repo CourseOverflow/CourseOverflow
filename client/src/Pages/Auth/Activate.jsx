d51657e0e2bdb90d4ccfbc4ac2cbd37564ec27d3
Pradyumna Singh <pradyumna.209301309@muj.manipal.edu>
2023-12-27 19:40:44 +0530

    user signup

--- client/src/Pages/Auth/Activate.jsx
+++ client/src/Pages/Auth/Activate.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Auth.module.css";
+import { connect } from "react-redux";
+import { verify } from "../../Actions/Auth";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-const Activate = () => {
-  return <div>Activate</div>;
+const Activate = ({ verify }) => {
+  const { uid, token } = useParams();
+  const [verified, setVerified] = useState(false);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    verify(uid, token);
+    setVerified(true);
+  };
+  const navigate = useNavigate();
+  if (verified) {
+    navigate("/CourseOverflow");
+  }
+  return (
+    <div className={styles["form-container"]}>
+      <h1 className={styles.authHeader}>Verify Your Account:</h1>
+      <hr className={styles.authLoader} />
+      <form
+        onSubmit={(e) => {
+          onSubmit(e);
+        }}
+        action="/"
+        method="post"
+      >
+        <div className={styles.authContainer}>
+          <button type="submit" className={styles.authButton}>
+            Verify
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default Activate;
+export default connect(null, { verify })(Activate);
+// export default Login;
--- client/src/Pages/Auth/Signup.jsx
+++ client/src/Pages/Auth/Signup.jsx
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import styles from "./Auth.module.css";
+import { signup } from "../../Actions/Auth";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
+const Signup = ({ signup, isAuthenticated }) => {
+  const [accountCreated, setAccountCreated] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
+    name: "",
     password: "",
+    re_password: "",
   });
-  const { email, password } = formData;
+  const { email, name, password, re_password } = formData;
+
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
   const onSubmit = async (e) => {
     e.prevnetDefault();
-    //login(emiail,password)
+    if (password !== re_password) {
+      console.log("Passwords do not match");
+    } else {
+      signup({ name, email, password, re_password });
+      setAccountCreated(true);
+    }
   };
 
-  // is the user authenticated?
-  // redirect them to the dashboard
-
-  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  if (isAuthenticated) {
+    navigate("/CourseOverflow"); // Navigate if authenticated
+    return null; // Or return something else if needed
+  }
+  if (accountCreated) {
+    navigate("/login");
+  }
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -53,6 +71,20 @@ const Signup = () => {
             className={styles.authInput}
             required
           />
+
+          <label htmlFor="name">
+            <b>Your Name</b>
+          </label>
+          <input
+            type="text"
+            placeholder="John Doe"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            className={styles.authInput}
+            required
+          />
+
           <label htmlFor="psw">
             <b>Password</b>
           </label>
@@ -68,7 +100,7 @@ const Signup = () => {
             <input
               type={showPassword ? "text" : "password"}
               placeholder="Enter Password"
-              name="psw"
+              name="password"
               value={password}
               onChange={(e) => onChange(e)}
               minLength={6}
@@ -83,18 +115,15 @@ const Signup = () => {
             <input
               type={showPassword ? "text" : "password"}
               placeholder="Enter Password"
-              name="cpsw"
+              name="re_password"
+              value={re_password}
+              onChange={(e) => onChange(e)}
+              minLength={6}
               required
               className={styles.authInput}
             />
           </div>
-          {/* <label className={styles.rememberBtn}>
-            <input type="checkbox" checked="checked" name="remember" /> Remember
-            me
-          </label>
-          <span className={styles.authPsw}>
-            <a href="/">Forgot password?</a>
-          </span> */}
+
           <button type="submit" className={styles.authButton}>
             Sign up for free
           </button>
@@ -124,5 +153,8 @@ const Signup = () => {
     </div>
   );
 };
+const mapStateToProps = (state) => ({
+  isAuthinticated: state.auth.isAuthenticated,
+});
 
-export default Signup;
+export default connect(mapStateToProps, { signup })(Signup);
