341a3eeca9b847d41df987d160fc034dd399b8c8
AnshUjlayan <anshujlayan3@gmail.com>
2024-01-28 13:28:17 +0530

    api config

--- client/src/Config/apiConfig.js
+++ client/src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEBUG = true;
+const DEBUG = false;
 const LOCAL_SERVER = "http://127.0.0.1:8000/api/";
 const REMOTE_SERVER = "https://course-overflow.vercel.app/api/";
 const baseURL = DEBUG ? LOCAL_SERVER : REMOTE_SERVER;
