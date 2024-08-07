7c68ce09609fb42d9837da12ceeb29d864417906
AnshUjlayan <anshujlayan3@gmail.com>
2024-08-07 12:53:48 +0530

    build files added

---
+++ server/build_file.sh
@@ -0,0 +1,7 @@
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py migrate
+python3.9 manage.py runserver
+echo "BUILD END"
+
